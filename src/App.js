9d0a650d237fbb8d0d09489a0138a49f83bbbb11
Rakha Djokosoetono <rakha.djokosoetono@student.manchester.ac.uk>
2022-01-22 02:33:53 +0000

    annoying warnings

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import ReactGA from "react-ga";
 import $ from "jquery";
 import "./App.css";
 import Header from "./Components/Header";
-import Footer from "./Components/Footer";
+/*import Footer from "./Components/Footer";
 import About from "./Components/About";
 import Resume from "./Components/Resume";
 import Contact from "./Components/Contact";
-import Portfolio from "./Components/Portfolio";
+import Portfolio from "./Components/Portfolio";*/
 
 class App extends Component {
   constructor(props) {
